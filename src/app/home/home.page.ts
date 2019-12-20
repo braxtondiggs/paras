5eeb4f556a337f5d90ad4b200621eea44378e970
Braxton Diggs <braxtondiggs@gmail.com>
2019-12-20 13:37:02 -0500

    Update calendar with data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,64 @@
-import { Component } from '@angular/core';
-import { CalendarComponentOptions } from 'ion2-calendar';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { CalendarComponentOptions, DayConfig, CalendarComponentMonthChange, CalendarComponent } from 'ion2-calendar';
+import * as moment from 'moment';
+import { DbService } from '../core/services';
+import { map } from 'rxjs/operators';
+import { Feed } from '../core/interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
   index = true; // TODO: Save using storage config
   date: string;
-  type: 'string';
   slideOpts = {
     allowTouchMove: false
   };
   calendarOpts: CalendarComponentOptions = {
-    from: new Date(2000, 0, 1)
+    from: new Date(2000, 0, 1),
+    daysConfig: []
   };
+  @ViewChild('calendar', { static: false }) cal: CalendarComponent;
 
-  constructor() { }
+  constructor(private db: DbService) { }
 
-  onChange($event) {
-    console.log($event);
+  onSelect($event) {
+    console.log($event); // TODO: open up detail view
+  }
+
+  ngAfterViewInit(): void {
+    this.getData(moment().startOf('month'), moment().endOf('month'));
+  }
+
+  onMonthChange($event: CalendarComponentMonthChange): void {
+    this.getData(moment($event.newMonth.dateObj), moment($event.newMonth.dateObj).endOf('month'));
+  }
+
+  private getData(start: moment.Moment, end: moment.Moment) {
+    this.db.collection$('feed', (ref) =>
+      ref
+        .where('date', '>=', start.toDate())
+        .where('date', '<', end.toDate()))
+      .pipe(
+        map((item: Feed[]) => item.filter(o => !o.active || !o.metered)))
+      .subscribe((items) => {
+        const daysConfig: DayConfig[] = items.map(item => ({
+          cssClass: this.getCalendarClass(item),
+          date: item.date.toDate(),
+          marked: true
+        }));
+        this.calendarOpts.daysConfig = daysConfig;
+        this.cal.options = this.calendarOpts;
+      });
+  }
+
+  private getCalendarClass(item: Feed): string | undefined {
+    if (item.active) {
+      return 'active';
+    } else if (item.metered) {
+      return 'metered';
+    }
   }
 }

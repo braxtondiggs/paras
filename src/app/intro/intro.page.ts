4476dccb2142adc2bf2f725c0f86d6f37e2cb7b7
Braxton Diggs <braxtondiggs@gmail.com>
2020-02-03 13:49:08 -0500

    Update intro & launcher icon

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
 })
-export class IntroPage implements OnInit {
+export class IntroPage {
+  constructor(private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  continue() {
+    localStorage.setItem('intro', 'true');
+    this.router.navigate(['/']);
   }
-
 }

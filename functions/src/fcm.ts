f1111b5d1f97ba41bdcfc99f94fd1883ff14bfe5
Braxton Diggs <braxtondiggs@gmail.com>
2022-02-08 15:20:58 +0000

    Remove icon from payload

--- functions/src/fcm.ts
+++ functions/src/fcm.ts
@@ -7,9 +7,7 @@ dayjs.extend(weekday);
 
 const payload: admin.messaging.MessagingPayload = {
   notification: {
-    title: 'Alternate Side Parking',
-    icon: 'notification_icon',
-    color: "#74b9ff"
+    title: 'Alternate Side Parking'
   }
 };
 export async function FCM(type?: string, tweet?: any) {
